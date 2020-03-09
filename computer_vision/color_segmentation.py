03f9450d95397f8b6790eacc371ebf8bfe082fcb
Samuel Ubellacker <subella@mit.edu>
2020-03-08 23:11:59 -0400

    working implementation

--- computer_vision/color_segmentation.py
+++ computer_vision/color_segmentation.py
@@ -35,8 +35,27 @@ def cd_color_segmentation(img, template):
 				(x1, y1) is the bottom left of the bbox and (x2, y2) is the top right of the bbox
 	"""
 	########## YOUR CODE STARTS HERE ##########
+        
+        # Convert BGR to HSV
+        hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
-	bounding_box = ((0,0),(0,0))
+        # define range of orange color in HSV
+        lower_orange = np.array([3,100,100])
+        upper_orange = np.array([18,255,255])
+
+        # Threshold the HSV image to get only orange colors
+        mask = cv2.inRange(hsv, lower_orange, upper_orange)
+
+        _, contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE) 
+
+        cont_sorted = sorted(contours, key=cv2.contourArea, reverse=True)
+    
+        x,y,w,h = cv2.boundingRect(cont_sorted[0])
+    
+        cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,255),5)
+        image_print(img)
+
+	bounding_box = ((x,y),(x+w,y+h))
 
 	########### YOUR CODE ENDS HERE ###########
 
