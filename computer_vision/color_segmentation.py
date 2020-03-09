87be8291eec2bd5f60088d1090dfcd5d8f6772cd
lisapeng <lisapeng@mit.edu>
2020-03-09 16:28:31 -0400

    sam's color segmentation.py added

--- computer_vision/color_segmentation.py
+++ computer_vision/color_segmentation.py
@@ -35,37 +35,27 @@ def cd_color_segmentation(img, template):
 				(x1, y1) is the bottom left of the bbox and (x2, y2) is the top right of the bbox
 	"""
 	########## YOUR CODE STARTS HERE ##########
+        
+        # Convert BGR to HSV
+        hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
-	bounding_box = ((0,0),(0,0))
-	
-	# Convert BGR to HSV
-	hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+        # define range of orange color in HSV
+        lower_orange = np.array([3,100,100])
+        upper_orange = np.array([18,255,255])
 
-    # define range of orange color in HSV
-	lower_orange = np.array([20,50,50])
-	upper_orange = np.array([40,255,255])
+        # Threshold the HSV image to get only orange colors
+        mask = cv2.inRange(hsv, lower_orange, upper_orange)
 
-	# Threshold the HSV image to get only orange colors
-    	mask = cv2.inRange(hsv, lower_orange, upper_orange)
+        _, contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE) 
 
-	#erosion
-	kernel = np.ones((5,5),np.uint8)
-	erosion = cv2.erode(mask,kernel,iterations = 1)
+        cont_sorted = sorted(contours, key=cv2.contourArea, reverse=True)
+    
+        x,y,w,h = cv2.boundingRect(cont_sorted[0])
+    
+        cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,255),5)
+        image_print(img)
 
-	#dilation
-	dilation = cv2.dilate(erosion,kernel,iterations = 1)
-
-	#find contours 
-	contours,hierarchy = cv2.findContours(dilation, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-
-	cnt = contours[0]
-
-	#bounding box
-	x,y,w,h = cv2.boundingRect(cnt)
-    	bounding_box=((x,y),(x+w,y+h))
-
-
-
+	bounding_box = ((x,y),(x+w,y+h))
 
 	########### YOUR CODE ENDS HERE ###########
 
